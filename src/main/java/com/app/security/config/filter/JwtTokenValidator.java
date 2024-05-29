952b6fff24a1e8898cab251caa4759d041b329fe
Andres Calderon <acaldup@gmail.com>
2024-05-29 18:55:18 -0500

    Ajustes JWT filter y agregado cookies de session

--- src/main/java/com/app/security/config/filter/JwtTokenValidator.java
+++ src/main/java/com/app/security/config/filter/JwtTokenValidator.java
@@ -4,6 +4,7 @@ import com.app.security.utils.JwtUtils;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -13,11 +14,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 
 @AllArgsConstructor
@@ -26,24 +27,23 @@ public class JwtTokenValidator extends OncePerRequestFilter {
     private JwtUtils jwtUtils;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         String jwtToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (jwtToken == null) {
+            if (request.getCookies() != null) {
+                jwtToken = "Bearer " + Arrays.stream(request.getCookies()).filter(cookie -> "auth_token".equals(cookie.getName())).map(Cookie::getValue).findFirst().orElse(null);
+            }
+        }
 
         if (jwtToken != null && jwtToken.startsWith("Bearer ")) {
             jwtToken = jwtToken.substring(7);
-
             DecodedJWT decodedJWT = jwtUtils.validateToken(jwtToken);
-
             String username = jwtUtils.extractUsername(decodedJWT);
             String stringAuthorities = jwtUtils.getSpecificClaim(decodedJWT, "authorities").toString();
-
             Collection<? extends GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(stringAuthorities);
-            SecurityContext context = SecurityContextHolder.getContext();
             Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
-            context.setAuthentication(authentication);
-            SecurityContextHolder.setContext(context);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
         }
 
         filterChain.doFilter(request, response);
--- src/main/java/com/app/security/controller/AuthenticationController.java
+++ src/main/java/com/app/security/controller/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.app.security.controller.dto.AuthCreateUserRequest;
 import com.app.security.controller.dto.AuthLoginRequest;
 import com.app.security.controller.dto.AuthResponse;
 import com.app.security.service.UserDetailServiceImpl;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,12 +23,18 @@ public class AuthenticationController {
     private UserDetailServiceImpl userDetailService;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthLoginRequest authRequest) {
-        return new ResponseEntity<>(this.userDetailService.loginUser(authRequest), HttpStatus.OK);
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthLoginRequest authRequest, HttpServletResponse response) {
+        var userDetails = this.userDetailService.loginUser(authRequest);
+        Cookie cookie = new Cookie("auth_token", userDetails.jwt());
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return new ResponseEntity<>(userDetails, HttpStatus.OK);
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<AuthResponse> register(@RequestBody @Valid AuthCreateUserRequest userRequest){
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid AuthCreateUserRequest userRequest) {
         System.out.println(userRequest.toString());
         return new ResponseEntity<>(this.userDetailService.createUser(userRequest), HttpStatus.CREATED);
     }
